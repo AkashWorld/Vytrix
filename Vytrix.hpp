e39acae232a6ffa8518b3488f297282cce3c8e9c
Khalid Akash <khalkash@gmail.com>
2018-08-10 01:12:58 -0400

    Added transpose

--- Vytrix.hpp
+++ Vytrix.hpp
@@ -186,13 +186,11 @@ class Vytrix{
                                   {matrix[0][1], matrix[1][1], matrix[2][1], matrix[3][1]},
                                   {matrix[0][2], matrix[1][2], matrix[2][2], matrix[3][2]},
                                   {matrix[0][3], matrix[1][3], matrix[2][3], matrix[3][3]}};
-        /*
         for(size_t i = 0; i < 4; ++i){
             for(size_t j = 0; j < 4; ++j){
                 placeholder[j][i] = matrix[i][j];
             }
         }
-        */
         memcpy(*matrix, *placeholder, sizeof(type)*16);
     }
 
